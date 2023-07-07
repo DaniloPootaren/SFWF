0e952d03bff9423c61e0c68de76b1bbeec3ef310
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@mns.mu>
2023-07-07 13:41:33 +0400

    Some changes

---
+++ src/api/scheme-template/routes/scheme-template.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * scheme-template router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::scheme-template.scheme-template');
---
+++ src/api/scheme-template/services/scheme-template.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * scheme-template service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::scheme-template.scheme-template');
